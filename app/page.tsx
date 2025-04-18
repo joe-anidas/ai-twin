efc7561fc3f1eba728c40c143b16d8b83b2c9f08
jayasurya0007 <115530104+jayasurya0007@users.noreply.github.com>
2025-04-18 21:21:47 +0530

    Modifed the page.tsx for vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import Navbar from "../components/Navbar"; // Adjust the import path as necessary
-import { useContract } from "@/context/ContractContext";
+// import { useContract } from "@/context/ContractContext";
 
 export default function Home() {
-  const { account } = useContract();
+  // const { account } = useContract();
   return (
     <main style={{ padding: "20px" }}>
       <Navbar /> {/* Use the Navbar component that already has connectWallet functionality */}
