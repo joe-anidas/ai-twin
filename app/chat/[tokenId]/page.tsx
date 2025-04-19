0477721e1204b2619af2d1a77fcffe6097907734
jayasurya0007 <115530104+jayasurya0007@users.noreply.github.com>
2025-04-19 11:31:04 +0530

    Added the async to ChatClient.tsx for vercel deployment

--- app/chat/[tokenId]/page.tsx
+++ app/chat/[tokenId]/page.tsx
@@ -1,6 +1,6 @@
 
 import ChatClient from './ChatClient';
 
-export default function ChatPage({ params }: { params: { tokenId: string } }) {
+export default async function ChatPage({ params }: { params: { tokenId: string } }) {
   return <ChatClient tokenId={params.tokenId} />;
 }
